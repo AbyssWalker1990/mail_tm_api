8c7e27a6ed65461a4d2cd584f84e908a1409f560
Vova <bordiian.v@gmail.com>
2022-12-28 11:02:24 +0200

    added comments

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ headers = {
     "Content-Type": "application/json"
 }
 
+# створює рандомну назву для пошти
 def create_random_name():
     symbols = string.ascii_letters + string.digits
     mail_name = ''
@@ -22,13 +23,19 @@ def create_mail(name):
         "address": name,
         "password": "lolsuprisedoll"
     }
-
     r = requests.post(base_url + "accounts", headers=headers, data=json.dumps(payload))
     time.sleep(2)
     print(r.status_code)
     response = r.json()
+    # поки просто виводить респонс з назвою створеного мейла
     print(response)
 
+# Тут буде запит для тогог щоб взяти токен
+def grab_token():
+    return "token"
+
+
+# Основна функція
 def main():
     print("WOrk")
     name = create_random_name()
